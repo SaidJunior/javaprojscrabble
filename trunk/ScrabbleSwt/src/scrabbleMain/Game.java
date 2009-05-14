c229294983b215cb94a4f24e8789ff4b11810108
royyadoo <royyadoo@dff76f2a-0e0f-11de-98bb-07cc9828235c>
2009-05-14 17:39:54 +0000

    initialization of letters score has been moved to gameDirectories.initGame. From now please insert all general initialization to this method.

--- trunk/ScrabbleSwt/src/scrabbleMain/Game.java
+++ trunk/ScrabbleSwt/src/scrabbleMain/Game.java
@@ -8,10 +8,9 @@ import Gui.MainWindow_ver2;
 public class Game{
 	public static void main(String[] args) { 
 		
-		gameDirectories.initDirectories();
-		
-		GameGui.createLetterScores();
-		
+		//  initialize all general needs of game: Directories, Records lists, Letter Score*/ 
+		gameDirectories.initGame();
+
 		char input = 0;
 		boolean validInput = false;
 		BufferedReader reader   = new BufferedReader(new InputStreamReader(System.in));
@@ -20,16 +19,16 @@ public class Game{
 			input = GetUserInput.getUserCharInput("Press 'c' for console application, 'g' for gui application or 'q' to quit.", reader);
 			switch(input) {
 			case 'c':	GameConsole.consoleMain();
-				        validInput = true;
-				        break;
+			validInput = true;
+			break;
 			case 'g':   MainWindow_ver2.startGuiGame();
-						validInput = true;
-						break;
+			validInput = true;
+			break;
 			case 'q':   System.out.println("Thank  you!!!");
-						validInput = true;
-						break;
+			validInput = true;
+			break;
 			default:    System.out.println("Not a valit input, try again.");
-				
+
 			}
 		} while (validInput == false);
 	}
