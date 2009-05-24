0553010879e63c2d90643749f7ed3f2756ce88a2
eviatarkhen@hotmail.com <eviatarkhen@hotmail.com@dff76f2a-0e0f-11de-98bb-07cc9828235c>
2009-05-24 13:43:18 +0000

    Networking 3/3. Finished game communication protocol

--- trunk/NetworkScrabble/src/server/MultiServer.java
+++ trunk/NetworkScrabble/src/server/MultiServer.java
@@ -3,27 +3,32 @@ package server;
 import gameUsers.UserDBQueries;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import comunicationProtocol.UserInfo;
 
 import database.UserQueriesImpl;
 
 public class MultiServer {
 	public static UserDBQueries userDB = new UserQueriesImpl();	
 	
-	private static Socket  waitSocket = null;  // Waiting user socket, shared between threads, private insures synchronized access 
-	public  static synchronized void   setWaitSocket(Socket newSocket){
+	private static SocketAndStreams  waitSocket = new SocketAndStreams();  // Waiting user socket, shared between threads, private insures synchronized access 
+	
+	
+	public  static synchronized void   setWaitSocket(Socket socket, ObjectInputStream in, ObjectOutputStream out){
 		/*DEBUG*/
-		if(waitSocket == null)
+		if(waitSocket.socket == null)
 			System.out.println("null socket changed!");
 		else
 			System.out.println("existing socket changed!");
 		/*END DEBUG*/
-		waitSocket = newSocket;
+		waitSocket.socket = socket;
+		waitSocket.in = in;
+		waitSocket.out = out;
 	}
-	public  static synchronized Socket getWaitSocket()                {return waitSocket;}
+	
+	public  static synchronized SocketAndStreams getWaitSocket()                {return waitSocket;}
 	
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
@@ -40,7 +45,7 @@ public class MultiServer {
         while (listening){
 			try {
 				playerSocket = serverSocket.accept();
-				new LoginPlayThread(playerSocket).start();
+				new LoginPlayThread_updated(playerSocket).start();
 				
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
@@ -52,6 +57,31 @@ public class MultiServer {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	
+	public static class SocketAndStreams {
+		private Socket socket = null;
+		private ObjectOutputStream out = null;
+        private ObjectInputStream  in  = null;
+        
+		public Socket getSocket() {
+			return socket;
+		}
+		public void setSocket(Socket socket) {
+			this.socket = socket;
+		}
+		public ObjectOutputStream getOut() {
+			return out;
+		}
+		public void setOut(ObjectOutputStream out) {
+			this.out = out;
+		}
+		public ObjectInputStream getIn() {
+			return in;
+		}
+		public void setIn(ObjectInputStream in) {
+			this.in = in;
 		}
 	}
 		
